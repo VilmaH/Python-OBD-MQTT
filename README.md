d1878444785f40d46ffa4736cab18b4cc9db3da5
Vilma Hallikas <VilmaH@github.com>
2018-09-29 23:55:18 +0300

    no message

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Python-OBD-MQTT
 Raspberry Pi Data logger for car sending data to a MQTT server
+
+This is a work in progress. I haven't even tested the first version yet.
+
+Connect an ELM327 OBD dongle to your Raspberry Pi either by USB or Bluetooth.
+Update config.ini accordingly.
+[FILE]
+enabled= do you want to save the data locally into a file generated each time car is switched on
+location= where to save said file, can be left blank to save in the same folder
+gps= save gps data to file?
+obd= save obd data to file?
+
+Same pretty much applies to [MQTT]
+
+Under [OBD] choose which information is asked from the car.
+If GET_DTC=yes DTC data is queried once during engine is turned on.
+If GET_CURRENT_DTC=yes DTC data is queried once when engine is turned off.
+If CLEAR_DTC=yes DTC is cleared after engine is turned on and DTC data is queried if GET_DTC was enabled.
---
+++ obdmqtt.py
@@ -0,0 +1,131 @@
+import configparser
+import obd
+import time
+import signal
+import sys
+import paho.mqtt.client as mqtt
+from datetime import datetime
+
+payload = ""
+config = configparser.ConfigParser()
+config.read('config.ini')
+fileconf = config['FILE']
+mqttconf = config['MQTT']
+obdconf = config['OBD']
+gpsconf = config['GPS']
+
+def handler(signum, frame):
+    print ("Exiting \n")
+    client.loop_stop()
+    connection.stop()
+    file.close()
+    sys.exit(0)
+
+#####################################################################################
+                        #MQTT Connection#
+#####################################################################################
+if config.getboolean(mqttconf['enabled']) == True:
+    # The callback for when the client receives a CONNACK response from the server.
+    def on_connect(client, userdata, flags, rc):
+        print("Connected to MQTT with result code "+str(rc))
+
+    def on_publish(client,userdata,result):             #create function for callback
+        print("Data published \n")
+
+    #Create MQTT Client
+    client = mqtt.Client(client_id=mqttconf['client_id']) 
+    client.on_connect = on_connect
+    client.on_publish = on_publish
+
+    #Check if we make a tls connection to MQTT server
+    if config.getboolean(mqttconf['tls_enabled']) == True:
+        client.tls_set(mqttconf(['ca_cert'])
+
+    #Create the MQTT Connection
+    client.connect(mqttconf['server'], int(mqttconf['port']))
+    client.loop_start()
+
+#####################################################################################
+                       #OBD Connection#
+#####################################################################################
+#Connect to OBD adapter
+connection = obd.Async()
+
+#Create OBD Command watchers
+connection.watch(obd.commands.RPM)
+connection.watch(obd.commands.SPEED)
+for x, y in obdconf.items():
+    if y == "yes":
+        connection.watch(obd.commands.x.upper())
+
+#Start OBD Connection
+connection.start()
+
+#####################################################################################
+    #Main loop for getting new data and publishing it to MQTT and to file#
+#####################################################################################
+
+while True:
+    #Start by getting RPM
+    r = connection.query(obd.commands.RPM).value.mangitude
+    #If configured, get DTC and clear DTC
+    if config.getboolean(obdconf['get_dtc']) == True:
+        d = connection.query(obd.commands.GET_DTC)
+        payload = "DTC:" + d + ","
+    if config.getboolean(obdconf['clear_dtc']) == True:
+        connection.query(obd.commands.CLEAR_DTC)
+
+    #If RPM = 0 engine is not running and we don't want to send data
+    while r > 0:
+        #Check if we should write to a file
+        if config.getboolean(fileconf['enabled']) == True:
+            #Check if we have initialized a file
+            if file == "":
+                #Open a file with current time as name
+                filename = fileconf['location'] + datetime.now() + ".log"
+                print("Opening file", filename, "for writing \n")
+                file = open(filename,w)
+
+        #Get rpm and speed
+        r = connection.query(obd.commands.RPM).value.mangitude
+        s = connection.query(obd.commands.SPEED).value.mangitude
+        payload = "RPM:" +r "," + "SPEED:" + s
+        #Listen to a kill signal
+        signal.signal(signal.SIGINT, handler)
+        
+        
+        for x, y in obdconf.items():
+            if y == "yes":
+                #Get all other configured OBD items and add them to the payload
+                payload = "," + x + ":" + connection.query(obd.commands.x.upper()).value.magnitude
+
+        if config.getboolean(mqttconf['enabled']) == True:
+            #Publish to MQTT if enabled
+            print("Publishing data \n")
+            publish= client.publish(mqttconf['topic'],payload)
+        
+        if config.getboolean(fileconf['enabled']) == True:
+            #Write data to file if enabled
+            print("Writing data to file \")
+            file.write(payload)
+
+        #Clear the payload for next run
+        payload = ""
+        #Sleep for the MQTT interval
+        time.sleep(int(mqttconf['publish_interval']))
+
+    #Engine shut down. Get DTC
+    if config.getboolean(obdconf['get_current_dtc']) == True:
+        dtc = connection.query(obd.commands.GET_CURRENT_DTC)
+        payload = "DTC:" + dtc
+        publish= client.publish(mqttconf['topic'],payload)
+        file.write(dtc)
+
+    if config.getboolean(fileconf['enabled']) == True:
+        print("Closing file \n")
+        file.close()
+        file = ""
+
+    print("Waiting for car to be switched on \n")
+    time.sleep(5)
+    signal.signal(signal.SIGINT, handler)
